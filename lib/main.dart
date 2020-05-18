7a1e78f83a8db3324870cc25ca67044a7237f3c8
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-05-18 10:39:36 -0600

    Starting the movies app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/page/home_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Películas',
+      initialRoute: '/',
+      routes: {
+        '/' : (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
