3648117022294a5f3da328514fb0adc651cb84c1
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-05-19 00:10:22 -0600

    Comsume JSON and show movies and images

--- lib/src/models/pelicula_model.dart
+++ lib/src/models/pelicula_model.dart
@@ -46,19 +46,28 @@ class Pelicula {
   });
 
   Pelicula.fromJsonMap(Map<String, dynamic> json) {
-    voteCount        = json['vote_count'];
-    id               = json['id'];
-    video            = json['video'];
-    voteAverage      = json['vote_average'] / 1;
-    title            = json['title'];
-    popularity       = json['popularity'] / 1;
-    posterPath       = json['poster_path'];
+    voteCount = json['vote_count'];
+    id = json['id'];
+    video = json['video'];
+    voteAverage = json['vote_average'] / 1;
+    title = json['title'];
+    popularity = json['popularity'] / 1;
+    posterPath = json['poster_path'];
     originalLanguage = json['original_language'];
-    originalTitle    = json['original_title'];
-    genreIds         = json['genre_ids'].cast<int>();
-    backdropPath     = json['backdrop_path'];
-    adult            = json['adult'];
-    overview         = json['overview'];
-    releaseDate      = json['release_date'];
+    originalTitle = json['original_title'];
+    genreIds = json['genre_ids'].cast<int>();
+    backdropPath = json['backdrop_path'];
+    adult = json['adult'];
+    overview = json['overview'];
+    releaseDate = json['release_date'];
+  }
+
+  getPosterImg() {
+    if (posterPath == null) {
+      return 'https://www.samsung.com/etc/designs/smg/global/imgs/support/cont/NO_IMG_600x600.png';
+    } else {
+      print(posterPath);
+      return 'https://image.tmdb.org/t/p/w500/$posterPath';
+    }
   }
 }
--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -3,6 +3,8 @@ import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
+  final peliculasProvider = new PeliculasProvider();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,11 +29,21 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperTarjetas() {
-    final pelicilasProvider = new PeliculasProvider();
-    pelicilasProvider.getEnCines();
-    
-    return CardSwiper(
-      peliculas: [1,2,3,4,5],
+    return FutureBuilder(
+      future: peliculasProvider.getEnCines(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return CardSwiper(
+            peliculas: snapshot.data,
+          );
+        } else {
+          return Container(
+              height: 400.0, child: Center(child: CircularProgressIndicator()));
+        }
+      },
     );
+
+    // final pelicilasProvider = new PeliculasProvider();
+    // pelicilasProvider.getEnCines();
   }
 }
