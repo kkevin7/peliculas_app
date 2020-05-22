005ec5bdf0b115cb7395ec0491140b58665bb03e
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-05-22 09:20:43 -0600

    Showing information

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
+  final peliculas = [
+    'Spiderman',
+    'Aquaman',
+    'Batman',
+    'Shazam!',
+    'IronMan',
+    'Capitan America',
+  ];
+
+  final peliculasRecientes = ['Spiderman', 'Capitan America'];
+
+  String seleccion = '';
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // Las acciones del AppBar
@@ -31,12 +44,38 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultados que se mostraran
-    return Container();
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Text(seleccion),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Son las sugerencias que aparecen cuando la persona escribe
-    return Container();
+
+    final listaSugerida = (query.isEmpty)
+        ? peliculasRecientes
+        : peliculas
+            .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context, i) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[i]),
+          onTap: () {
+            seleccion = listaSugerida[i];
+            showResults(context);
+          },
+        );
+      },
+    );
   }
 }
